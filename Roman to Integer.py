45196b1c574bbd1cf2aa770c9b0fccab877cfc1a
Komal Patil <ku.patil31@gmail.com>
2019-11-05 22:54:43 -0800

    ROman to Integer leetcode python solution added

---
+++ Roman to Integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_value = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,   "D": 500, "M": 1000}
+        numerical_value = 0
+        for i in range(len(s)):
+            if i > 0 and roman_value[s[i]] > roman_value[s[i-1]]:
+                numerical_value += roman_value[s[i]] -2 * roman_value[s[i-1]]
+            else: 
+                numerical_value += roman_value[s[i]]
+        return numerical_value
