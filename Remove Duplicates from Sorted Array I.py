e29c0c636461e494bdbe4627cde18f869be321f0
komalupatil <ku.patil31@gmail.com>
2019-11-22 19:02:02 -0800

    added leetcode solution for remove duplicates from sorted array I

---
+++ Remove Duplicates from Sorted Array I.py
@@ -0,0 +1,17 @@
+#leetcode26
+class removeduplicates:
+    def rem_dup(self, nums):
+        slow = 0 #slow pointer
+        fast = 1 #fast pointer
+        while(fast< len(nums)):
+            if nums[slow] != nums[fast]:
+                slow +=1 
+                nums[slow] = nums[fast]
+            else:
+                fast +=1
+        return slow+1 #takes only the length of actual non duplicate numbers
+nums1 = [0,0,1,1,1,2,2,3,3,4]
+removeDuplicates = removeduplicates()
+result = removeDuplicates.rem_dup(nums1)
+print(result)
+print(nums1[0:result])
