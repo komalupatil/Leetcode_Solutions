41a5b03fc298e7b662f23b7fe15b5c0f768d05f2
komalupatil <ku.patil31@gmail.com>
2019-11-23 18:31:22 -0800

    added leetcode solution for binary search algorithm

---
+++ Binary Search Algorithm.py
@@ -0,0 +1,15 @@
+#leetcode 704
+#time complexity log2(n) +1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) -1
+        while(start <= end):
+            mid = (start + end)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid -1
+            else:
+                start = mid+1
+        return -1
