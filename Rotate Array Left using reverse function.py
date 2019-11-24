848a8441c2941ff7dd2b346c08deec419ac9f773
komalupatil <ku.patil31@gmail.com>
2019-11-23 20:05:47 -0800

    added leetcode solution to rotate array from left using reverse function

---
+++ Rotate Array Left using reverse function.py
@@ -0,0 +1,21 @@
+class Solution:
+    
+    def rev(self,nums,s,e):
+        while(s<e):
+            temp = nums[s]
+            nums[s] = nums[e]
+            nums[e] = temp
+            s +=1
+            e -=1
+
+    def rotate_array_left(self,nums,k):
+        k = k% len(nums)
+        self.rev(nums,0,k-1)
+        self.rev(nums, k, len(nums)-1)
+        self.rev(nums, 0, len(nums)-1)
+        return nums
+solution = Solution()
+nums1=[1,2,3,4,5,6,7]
+k1=3
+result = solution.rotate_array_left(nums1,k1)
+print(result)
