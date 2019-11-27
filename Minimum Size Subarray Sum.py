cd45acc0ad5bcc3e1895ae12504b059333bc434f
komalupatil <ku.patil31@gmail.com>
2019-11-26 21:14:50 -0800

    Leetcode solution for minimum size subarray sum added

---
+++ Minimum Size Subarray Sum.py
@@ -0,0 +1,22 @@
+#leetcode 209
+class Solution:
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        windowStart = 0
+        windowSum = 0.0
+        minLength = float('inf')
+        for windowEnd in range(len(nums)):
+            windowSum += nums[windowEnd]
+            print("windowSum ", windowSum)
+            while(windowSum >= s):
+                minLength = min(minLength, windowEnd-windowStart+1)
+                print(minLength)
+                windowSum = windowSum - nums[windowStart]
+                windowStart += 1
+                
+        if minLength == float('inf'): #for conditions like s=3, [1,1] where it wont go into while loop and minLength will be infinite
+            return 0
+        else:
+            
+            return minLength
