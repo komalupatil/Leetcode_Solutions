93d3ea32a0a25e57bfb075cd0aa882ac23683881
komalupatil <ku.patil31@gmail.com>
2019-12-02 22:29:27 -0800

    Leetcode solution for Maximum subarray for 0(n) optimized added

--- Maximum Subarray.py
+++ Maximum Subarray.py
@@ -1,11 +1,12 @@
+#leetcode 53
+#o(n) solution
 class Solution:
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        sum = max_sum = nums[0]
-        for i in range(1, len(nums)):
-            sum = max(nums[i], nums[i] + sum)
-            max_sum = max(sum, max_sum)
-        return max_sum
+    def maxSubArray(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        maxEndingSoFar = nums[0]
+        maxGlobal =nums[0]
+        for i in range(1,len(nums)):
+            maxEndingSoFar = max(nums[i], maxEndingSoFar + nums[i])
+            maxGlobal = max(maxEndingSoFar, maxGlobal)
+        return maxGlobal
