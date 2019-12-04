cd2f8cb247828e390b3a412cb581ca2d51c8ae78
komalupatil <ku.patil31@gmail.com>
2019-12-03 23:05:45 -0800

    Leetcode solution for group anagram using hahmap added

---
+++ Group Anagram.py
@@ -0,0 +1,11 @@
+#leetcode 49
+#using dictionary of lists
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = collections.defaultdict(list)
+        for string in strs:
+            count = [0] * 26
+            for ch in string:
+                count[ord(ch) - ord('a')] += 1
+            d[tuple(count)].append(string)
+        return d.values()
