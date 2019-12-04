4c280e2458abc7710a61c4d407915d9d071917f5
komalupatil <ku.patil31@gmail.com>
2019-12-03 22:22:08 -0800

    Leetcode solution for valid anagram using sorted function added

---
+++ Valide Anagram using sorted function.py
@@ -0,0 +1,10 @@
+#leetcode 242
+# using sort function
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False
