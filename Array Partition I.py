fc086b2b2122b0c8e50b16ae74b377003b741647
komalupatil <ku.patil31@gmail.com>
2019-12-14 00:01:59 -0800

    Leetcode solution for Array Partition I added

---
+++ Array Partition I.py
@@ -0,0 +1,9 @@
+#leetcode 561
+#sort the array so that alternate elements will be min of the addition of its adjecent element and all those alternates element addition will be the min sum
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        min_sum = 0
+        for i in range(0,len(nums), 2):
+            min_sum = min_sum + nums[i]
+        return min_sum
