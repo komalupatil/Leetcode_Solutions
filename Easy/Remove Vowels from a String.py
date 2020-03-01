e63ef3f8bf4bee691faf18e32c32acfa4c6039d3
komalupatil <ku.patil31@gmail.com>
2020-03-01 15:13:50 -0800

    Leetcode solution for Remove Vowels form a String Added

---
+++ Easy/Remove Vowels from a String.py
@@ -0,0 +1,15 @@
+#Leetcode1119. Remove Vowels from a String
+
+class Solution(object):
+    def removeVowels(self, S):
+        newS = ""
+        vowel_set = {'a', 'e', 'i', 'o', 'u'}
+        for i in S:
+            if i not in vowel_set:
+                newS += i
+        return newS
+
+solution = Solution()
+string = solution.removeVowels("leetcodeisacommunityforcoders")
+print(string)
+            
