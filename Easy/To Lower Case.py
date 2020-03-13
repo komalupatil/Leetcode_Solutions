8c36df038e1dd6520a49ebf1a2701cbdf7b8f1c7
komalupatil <ku.patil31@gmail.com>
2020-03-13 00:09:33 -0700

    Leetcode Solution for To Lower Case Added

---
+++ Easy/To Lower Case.py
@@ -0,0 +1,24 @@
+# Leetcode 709. To Lower Case
+#Solution1
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
+
+#Solution2
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        char = []
+        for c in str:
+            if "A" <= c <= "Z":
+                char.append(chr(ord(c)+32))
+            else:
+                char.append(c)
+        return "".join(c for c in char)
+
+#Solution3
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return "".join(chr(ord(c)+32) if "A" <= c <= "Z" else c for c in str)
