c975b26b5babf1e5e99d0427fe9a6abd53bee36d
komalupatil <ku.patil31@gmail.com>
2020-03-13 00:39:13 -0700

    Leetcode Solution for Single-Row Keyboard added

---
+++ Easy/Single-Row Keyboard.py
@@ -0,0 +1,25 @@
+#leetcode 1165. Single-Row Keyboard
+
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        j = 0
+        time = 0
+        lookup = {val:i for i, val in enumerate(keyboard)}
+        for  c in word:
+            time += abs(lookup[c] - j)
+            j = lookup[c]
+        return time
+
+#solution2 - without using dictionary
+
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        cur_index = 0
+        time = 0
+        for  c in word:
+            next_index = keyboard.index(c)
+            time += abs(next_index - cur_index)
+            cur_index = next_index
+        return time
+
+
