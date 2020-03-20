97593001e8c669ee0b95edfd13631549fe0dedd2
komalupatil <ku.patil31@gmail.com>
2020-03-19 23:05:56 -0700

    added leetcode solution for Robot Return to Origin

---
+++ Easy/Robot Return to Origin.py
@@ -0,0 +1,6 @@
+#Leetcode 657. Robot Return to Origin
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        return moves.count('L') == moves.count('R') and moves.count('U') == moves.count('D')
+        
