a0c7c5c8b79c21c979be475d7316be4691102164
komalupatil <ku.patil31@gmail.com>
2020-03-20 00:22:34 -0700

    added leetcode solution for Self Dividing Numbers

---
+++ Easy/Self Dividing Numbers.py
@@ -0,0 +1,39 @@
+#Leetcode 728. Self Dividing Numbers
+
+#Solution1
+
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        result= []
+        for i in range(left, right+1):
+            if '0' in str(i):
+                continue
+            s = True
+            for j in str(i):
+                if i % int(j) != 0:
+                    s = False
+                    break
+            if s == True:
+                result.append(i)
+        return result
+
+
+
+#Solution2
+
+
+def is_self_dividing(x):
+        s = str(x)
+        for i in s:
+            if i=="0" or x%int(i)!=0:
+                return False
+        return True
+class Solution:
+    
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        result = []
+        for num in range(left, right+1):
+            if is_self_dividing(num):
+                result.append(num)
+        return result
+                
