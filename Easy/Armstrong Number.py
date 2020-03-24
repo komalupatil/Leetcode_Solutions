b595411c12642f8a1d6e56cef2723996b73faab1
komalupatil <ku.patil31@gmail.com>
2020-03-24 01:05:20 -0700

    added leetcode solution for Armstrong number

---
+++ Easy/Armstrong Number.py
@@ -0,0 +1,33 @@
+#Leetcode 1134. Armstrong Number
+
+
+#Solution1
+
+
+class Solution:
+    def isArmstrong(self, N: int) -> bool:
+        result = 0
+        for i in str(N):
+            result += int(i) ** len(str(N)))  #or using pow() function from import math
+        return result == N
+        
+            
+obj = Solution()
+result = obj.isArmstrong(153)
+result1 = obj.isArmstrong(123)
+print(result)
+print(result1)
+
+
+#Solution2
+
+class Solution:
+    def isArmstrong(self, N: int) -> bool:
+        temp = N
+        total = 0
+        k = len(str(N))  #another way to calculate length of given number is "k = math.floor(math.log10(N)) + 1 ".
+        while temp:
+            digit = temp%10
+            total += digit ** k
+            temp //= 10
+        return total == N
