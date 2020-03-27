505d678c762777d1557ec1c8655a6f05d1fdaef9
komalupatil <ku.patil31@gmail.com>
2020-03-27 01:37:29 -0700

    added solution for missing number problem using cyclic sort

--- Easy/Missing Number.py
+++ Easy/Missing Number.py
@@ -46,3 +46,22 @@ class Solution:
         total = ((len(nums)+1)*(len(nums)))/2
         missing_num = total - list_total
         return int(missing_num)
+
+
+#Solution4 using cyclic sort (time complexity o(n) and space complexity o(1))
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums):
+            j = nums[i]
+            if nums[i] < len(nums) and nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i+=1
+        
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
+        return len(nums)
