66c86a25e421ac252f5bd54e48fafce6a692c502
komalupatil <ku.patil31@gmail.com>
2020-04-02 19:03:20 -0700

    added leetcode solution for most common words

---
+++ Easy/Most Common Word.py
@@ -0,0 +1,22 @@
+#Leetcode 819. Most Common Word
+
+#Solution
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        for c in "!?',;.":
+            paragraph = paragraph.replace(c, " ")
+        dict1 = dict()
+        count = 0
+        result = " "
+        for word in paragraph.lower().split():
+            if word in banned:
+                continue
+            elif word in dict1:
+                dict1[word] +=1
+            else:
+                dict1[word] = 1
+            if dict1[word] > count:
+                count = dict1[word]
+                result = word
+        return result
