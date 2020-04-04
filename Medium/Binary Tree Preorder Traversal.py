19292b8e67caf753a1f13adc9574878cfaac91ff
komalupatil <ku.patil31@gmail.com>
2020-04-04 01:54:02 -0700

    adding iterative solution for preorder traversal

--- Medium/Binary Tree Preorder Traversal.py
+++ Medium/Binary Tree Preorder Traversal.py
@@ -21,3 +21,26 @@ class Solution:
             output.append(root.val)
             self.preorder(root.left, output)
             self.preorder(root.right, output)
+
+
+
+#Solution2 iterative:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        result = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                result.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return result
