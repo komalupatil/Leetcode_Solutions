556d09932a9fbc43f8419aec2b5bdf3f02ddbff4
komalupatil <ku.patil31@gmail.com>
2020-04-10 01:08:26 -0700

    adding solution using 2 stacks for min stack

--- Easy/Min Stack.py
+++ Easy/Min Stack.py
@@ -1,5 +1,7 @@
 #leetcode155 Min Stack
 
+#Solution1
+
 class MinStack:
 
     def __init__(self):
@@ -34,6 +36,43 @@ class MinStack:
             return self.q[len(self.q)-1][1]
         
 
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+
+#Solution2 - using two stacks 
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minStack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if len(self.minStack)==0 or x <= self.minStack[-1]:
+            self.minStack.append(x)
+                
+    def pop(self) -> None:
+        last = self.stack.pop()
+        if last == self.minStack[-1]:
+            self.minStack.pop()
+               
+    def top(self) -> int:
+        return self.stack[-1]
+        
+    def getMin(self) -> int:
+        return self.minStack[-1]
+        
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
