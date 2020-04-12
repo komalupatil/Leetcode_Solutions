04ffba2572bcbacdf97f1402ad0ff72b7c196378
komalupatil <ku.patil31@gmail.com>
2020-04-12 01:54:19 -0700

    added insertion sort implementation

---
+++ Easy/Insertion Sort.py
@@ -0,0 +1,14 @@
+class Sort:
+    def insertionSort(self, arr):
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i-1
+            while j>=0 and key<arr[j]:
+                arr[j+1] = arr[j]
+                j -= 1
+            arr[j+1] = key
+        return arr
+obj = Sort()
+arr1=[12,11,13,5,6]
+sortedArray = obj.insertionSort(arr1)
+print(sortedArray)
