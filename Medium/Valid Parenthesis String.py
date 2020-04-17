5a51f708f7c9ad6a8455e7d216bc37dcbfd31dcc
komalupatil <ku.patil31@gmail.com>
2020-04-17 00:36:47 -0700

    added solution for valid paranthesis string

---
+++ Medium/Valid Parenthesis String.py
@@ -0,0 +1,76 @@
+#Leetcode 678. Valid Parenthesis String
+
+
+#Solution1
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        if len(s) == 0 or s== "*":
+            return True
+        if s == "(" or s == ")":
+            return False
+        left_count = 0
+        for i in s:
+            if i == ")":
+                left_count -= 1
+            else:
+                left_count +=1
+            if left_count < 0:
+                return False
+        if left_count == 0:
+            return True
+        
+        right_count = 0
+        for i in reversed(s):
+            if i == "(":
+                right_count -= 1
+            else:
+                right_count +=1
+            if right_count < 0:
+                return False
+        return True
+
+
+#Solution2 - Greedy Approach
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        if len(s) == 0:
+            return True
+        left_count = 0
+        right_count = 0
+        for i in range(len(s)):
+            left_count +=1 if s[i] in "(*" else -1
+            right_count +=1 if s[len(s)-i-1] in ")*" else -1
+            if left_count < 0 or right_count < 0:
+                return False
+    
+        return True
+
+#Solution3 - using 2 stacks
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        stack1 = []
+        stack2 = []
+        for index, value in enumerate(s):
+            if value == "(":
+                stack1.append(index)
+            elif value == "*":
+                stack2.append(index)
+            else:
+                if len(stack1) >0:
+                    stack1.pop()
+                elif len(stack2) > 0:
+                    stack2.pop()
+                else:
+                    return False
+        while stack1 and stack2:
+            if stack1[-1] < stack2[-1]:
+                stack1.pop()
+                stack2.pop()
+            else:
+                break
+        return len(stack1) == 0
+            
+
