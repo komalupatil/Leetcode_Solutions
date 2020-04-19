18df571983daf5403fe9dcb803a9545661594469
komalupatil <ku.patil31@gmail.com>
2020-04-19 02:05:10 -0700

    added solution for sort char by freq

---
+++ Medium/Sort Characters By Frequency.py
@@ -0,0 +1,29 @@
+#Leetcode 451. Sort Characters By Frequency
+
+#Solution - using bucket sort - similar to Top K frequent elements
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        
+        if len(s) == 0:
+            return ""
+        
+        d = {}
+        
+        for char in s:
+            if char in d:
+                d[char] +=1
+            else:
+                d[char] = 1
+        
+        buckets = [[] for _ in s]
+        
+        
+        for key, value in d.items():
+            buckets[value-1].append(key*value) #to print the char by freq doing key*value here
+        
+        res = ""
+        for bucket in buckets[::-1]:
+            for char in bucket:
+                res += char
+        return res
