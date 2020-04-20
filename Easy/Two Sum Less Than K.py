4f70fb33566510cd73be54d809bce9690cf0c556
komalupatil <ku.patil31@gmail.com>
2020-04-20 01:29:01 -0700

    added solution for two sum less than k

---
+++ Easy/Two Sum Less Than K.py
@@ -0,0 +1,27 @@
+class Solution:
+    def twoSumLessThanK(self, nums, k):
+        maximum = -1
+        l, r = 0, len(nums)-1
+        
+        nums = sorted(nums)
+        
+        while (l < r):
+            if nums[l] + nums[r] == k:
+                return k
+            elif (nums[l] + nums[r]) > k:
+                r -=1
+            else:
+                maximum = max(maximum, nums[l]+nums[r])
+                l +=1
+        return maximum
+
+obj = Solution()
+nums1 = [34,23,1,24,75,33,54,8]
+k1 = 60
+result1 = obj.twoSumLessThanK(nums1, k1)
+print(result1)
+A = [10,20,30]
+K = 15
+result2 = obj.twoSumLessThanK(A, K)
+print(result2)
+        
