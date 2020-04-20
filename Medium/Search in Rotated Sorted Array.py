ef806d539a50bb7f85fa705640ec84b35a42bb80
komalupatil <ku.patil31@gmail.com>
2020-04-19 23:14:55 -0700

    added solution for search in rotated sorted array

---
+++ Medium/Search in Rotated Sorted Array.py
@@ -0,0 +1,29 @@
+#Leetcode 33. Search in Rotated Sorted Array
+
+#Solution - using binary search
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left = 0
+        right = len(nums) -1
+        
+        while left <= right:
+            
+            mid = left + (right-left)//2
+            
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] < nums[right]:
+                if target > nums[mid] and target <= nums[right]:
+                    left = mid +1
+                else:
+                    right = mid -1
+            else:
+                if target < nums[mid] and target >= nums[left]:
+                    right = mid-1
+                else:
+                    left = mid+1
+        return -1
+            
