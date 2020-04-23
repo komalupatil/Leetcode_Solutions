f5392f123c8235f5861a13f282bd14b446b4b5f5
komalupatil <ku.patil31@gmail.com>
2020-04-22 23:15:03 -0700

    added soln for leetcode 560 Subarray Sum Equals K

---
+++ Medium/Subarray Sum Equals K.py
@@ -0,0 +1,24 @@
+#Leetcode 560. Subarray Sum Equals K
+
+#Solution
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
+        count,contSum = 0, 0
+        
+        d = {}
+        d[0] = 1 #taking this as contiguous sum start with 0
+        
+        for i in range(len(nums)):
+            contSum += nums[i]
+            if contSum-k in d:
+                count += d[contSum-k] #difference between sum and k means that subarray exists
+            if contSum in d:
+                d[contSum] +=1
+            else:
+                d[contSum] = 1
+            
+                
+        return count                
+                
