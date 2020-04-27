c2962b84508a1bac391571a442f2d8d45815e315
komalupatil <ku.patil31@gmail.com>
2020-04-26 23:28:43 -0700

    added solution for Leetcode 1143. Longest Common Subsequence

---
+++ Medium/Longest Common Subsequence.py
@@ -0,0 +1,17 @@
+#Leetcode 1143. Longest Common Subsequence
+
+#Solution - using DP
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n,m = len(text1), len(text2)
+        maxlength = 0
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if text1[i-1]==text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]) 
+                maxlength = max(maxlength, dp[i][j])
+        return maxlength
