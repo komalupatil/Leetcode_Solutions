2c6d1e2a681c5de0d4918924cd59c2aef15e1d0d
komalupatil <ku.patil31@gmail.com>
2020-04-27 23:37:21 -0700

    added solution for Leetcode 221. Maximal Square

---
+++ Medium/Maximal Square.py
@@ -0,0 +1,22 @@
+#Leetcode 221. Maximal Square
+
+
+#Solution
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        m= len(matrix)   
+        if m == 0:
+            return 0
+        n= len(matrix[0])
+        
+        height = 0
+        
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if matrix[i-1][j-1] == "1":
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+                    height = max(height, dp[i][j])
+        return height**2
