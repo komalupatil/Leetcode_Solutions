00bcf8c3896869c8610f0510c4fecb34e67c2962
komalupatil <ku.patil31@gmail.com>
2020-04-29 23:39:36 -0700

    added solution for Leetcode 124. Binary Tree Maximum Path Sum

---
+++ Hard/Binary Tree Maximum Path Sum.py
@@ -0,0 +1,23 @@
+#Leetcode 124. Binary Tree Maximum Path Sum
+
+
+#Solution
+
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+    
+        self.maxpathSum = float('-inf')
+        
+        def depth(node):
+            
+            if node == None:
+                return 0
+            
+            left = max(0, depth(node.left))
+            right = max(0, depth(node.right))
+            currentmaxPath=node.val+left+right
+            self.maxpathSum = max(self.maxpathSum, currentmaxPath)
+
+            return (max(left,right) + node.val)
+        depth(root)
+        return self.maxpathSum
