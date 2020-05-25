dab680011532093b218b9b3cad7e8986b77cbd91
komalupatil <ku.patil31@gmail.com>
2020-05-24 23:47:50 -0700

    added solution for Leetcode 236. Lowest Common Ancestor of a Binary Tree

---
+++ Medium/Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,31 @@
+#Leetcode 236. Lowest Common Ancestor of a Binary Tree
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == None:
+            return None
+        
+        if (root.val == p.val or root.val == q.val):
+            return root
+        
+        left = TreeNode(None)
+        right = TreeNode(None)
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left == None:
+            return right
+        
+        if right == None:
+            return left
+        
+        return root
