00106f27bee4b87e03206f9f3ee67a367888ea9a
komalupatil <ku.patil31@gmail.com>
2020-10-24 15:51:34 -0700

    added solution to intervals problems

--- Medium/Interval List Intersections.py
+++ Medium/Interval List Intersections.py
@@ -1,6 +1,6 @@
 #Leetcode 986. Interval List Intersections
 
-#Solution
+#Solution1
 
 
 class Solution:
@@ -22,3 +22,26 @@ class Solution:
                 b+=1
             
         return intervals
+
+
+#Solution2
+
+class Solution:
+    def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        i,j = 0,0
+        result = []
+        
+        while i< len(A) and j<len(B):
+            a_overlaps_b = A[i][0] <= B[j][0] and B[j][0] <= A[i][1]
+            b_overlaps_a = B[j][0] <= A[i][0] and A[i][0] <= B[j][1]
+            
+            if (a_overlaps_b or b_overlaps_a):
+                result.append([max(A[i][0], B[j][0]), min(A[i][1], B[j][1])])
+                
+            if A[i][1] < B[j][1]:
+                i+=1
+            else:
+                j+=1
+            
+        return result
+                
---
+++ Medium/Merge Intervals.py
@@ -0,0 +1,27 @@
+#Leetcode 56. Merge Intervals
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if len(intervals) <2:
+            return intervals
+        
+        mergedIntervals = []
+        
+        intervals.sort(key=lambda x:x[0])
+        
+        start = intervals[0][0]
+        end = intervals[0][1]
+        
+        for i in range(1, len(intervals)):
+            interval = intervals[i]
+            if interval[0] <=end:
+                end = max(interval[1], end)
+            else:
+                mergedIntervals.append((start,end))
+                start = interval[0]
+                end = interval[1]
+        mergedIntervals.append((start, end))
+        
+        return mergedIntervals
+            
