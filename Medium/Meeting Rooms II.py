2511a9b65305769963a95dd9e6c2831f7f19aa05
komalupatil <ku.patil31@gmail.com>
2020-10-24 22:05:01 -0700

    meeting rooms II solution added

--- Medium/Meeting Rooms II.py
+++ Medium/Meeting Rooms II.py
@@ -1,3 +1,19 @@
 #Leetcode 253. Meeting Rooms II
 
-
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        intervals.sort(key=lambda x:x[0])
+        
+        minRooms = 0
+        minHeap = []
+        
+        for interval in intervals:
+            while (len(minHeap) > 0 and interval[0] >= minHeap[0]):
+                heapq.heappop(minHeap)
+            
+            heapq.heappush(minHeap, interval[1])
+            
+            minRooms = max(minRooms, len(minHeap))
+            
+        return minRooms
