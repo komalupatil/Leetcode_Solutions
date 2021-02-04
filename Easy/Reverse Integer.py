25663b7b9b06733107ad6ab371ca7b06bc3f53b5
komalupatil <ku.patil31@gmail.com>
2021-02-03 22:43:22 -0800

    adding solution for leetcode problem 7

--- Easy/Reverse Integer.py
+++ Easy/Reverse Integer.py
@@ -1,3 +1,6 @@
+#Leetcode 7. Reverse Integer
+
+#Solution1
 class Solution:
     def reverse(self, x):
         if x < 0:
@@ -12,3 +15,16 @@ class Solution:
         else:
 
             return int(rev)
+
+#Solution2 *
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x < 0:
+            return -1 * self.reverse(-x)
+        result = 0
+        while x != 0:
+            digit = x % 10
+            result = result * 10 + digit
+            x = int(x / 10)
+			
+        return 0 if result > pow(2, 31) - 1 or result < -pow(2, 31) else result
