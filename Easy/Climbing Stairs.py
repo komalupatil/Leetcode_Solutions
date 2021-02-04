2f4735b9116e6db4a726d5721bc2f6627e897391
komalupatil <ku.patil31@gmail.com>
2021-02-03 23:02:54 -0800

    adding solution of leetcode 70

---
+++ Easy/Climbing Stairs.py
@@ -0,0 +1,13 @@
+#Leetcode 70. Climbing Stairs
+
+#Solution - DP bottom up approach (can be solved by recursion)
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        res = [0 for i in range(n)]
+        res[0], res[1] = 1,2
+        
+        for i in range(2,n):
+            res[i] = res[i-1] + res[i-2]
+        return res[-1]
