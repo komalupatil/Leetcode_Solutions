78c048fd23900efdb07c672ff67247bec1d2acce
komalupatil <ku.patil31@gmail.com>
2021-02-27 23:54:26 -0800

    4Sum variations leetcode problem solutions added

---
+++ Medium/4Sum II.py
@@ -0,0 +1,18 @@
+#Leetcode 454. 4Sum II
+
+#Solution
+
+class Solution:
+    def fourSumCount(self, A: List[int], B: List[int], C: List[int], D: List[int]) -> int:
+        table = {}
+        
+        for a in A:
+            for b in B:
+                table[a+b] = table.get(a+b,0)+1
+        
+        count = 0
+        for c in C:
+            for d in D:
+                if (0-c-d) in table:
+                    count += table[0-c-d]
+        return count
