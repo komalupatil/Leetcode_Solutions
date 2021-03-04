7a382626b2f121c8d308b8e81bf338976b1ad78d
komalupatil <ku.patil31@gmail.com>
2021-03-03 23:58:06 -0800

    added solution for leetcode 234

---
+++ Easy/Palindrome Linked List.py
@@ -0,0 +1,34 @@
+#Leetcode 234. Palindrome Linked List
+
+#Topic: Two Pointers
+
+#Solution
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        
+        fast = slow = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        prev = None
+        
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+        
+        while prev:
+            if prev.val != head.val:
+                return False
+            prev = prev.next
+            head = head.next
+        return True
