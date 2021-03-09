482c0c58dee1c9384c35bc0145e4379342d5e59c
komalupatil <ku.patil31@gmail.com>
2021-03-08 19:26:47 -0800

    solution added for leetcode 905

---
+++ Easy/Sort Array By Parity.py
@@ -0,0 +1,46 @@
+#Leetcode 905. Sort Array By Parity
+
+#Topic: two pointers
+
+#Solution1
+
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        
+        if len(A) == 0:
+            return 0
+        
+        p1 = 0
+        p2 = len(A)-1
+        
+        while p1<p2:
+            if A[p1]%2==0:
+                p1+=1
+                continue
+            if A[p2]%2!=0:
+                p2-=1
+                continue
+            if p1<p2:
+                A[p1], A[p2] = A[p2], A[p1]
+                p1+=1
+                p2-=1
+            
+        return A
+            
+#Solution2
+class Solution:
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        
+        if len(A) == 0:
+            return 0
+        
+        p1 = 0
+        p2 = len(A)-1
+        
+        while p1<=p2:
+            if A[p1]%2==0:
+                p1+=1
+            else:
+                A[p1], A[p2] = A[p2], A[p1]
+                p2-=1
+        return A
