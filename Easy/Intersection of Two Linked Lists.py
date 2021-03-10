82232ced68906d57944d46803ab4ed0ffccd3e5b
komalupatil <ku.patil31@gmail.com>
2021-03-09 23:38:24 -0800

    leetcode 160 solution using hashset added

--- Easy/Intersection of Two Linked Lists.py
+++ Easy/Intersection of Two Linked Lists.py
@@ -1,6 +1,6 @@
 #Leetcode 160. Intersection of Two Linked Lists
 
-#Solution
+#Solution1
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -17,3 +17,28 @@ class Solution:
             pA = headB if pA is None else pA.next
             pB = headA if pB is None else pB.next
         return pA
+
+
+#Solution2
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        
+        s = set()
+        
+        while headA != None:
+            s.add(headA)
+            headA = headA.next
+        
+        while headB != None:
+            if headB in s:
+                return headB
+            headB = headB.next
+        
+        return None
