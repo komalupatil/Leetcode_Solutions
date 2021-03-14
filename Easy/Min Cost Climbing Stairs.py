adaaebb59a57a14b5dbeca85f75c0072b899025a
komalupatil <ku.patil31@gmail.com>
2021-03-13 18:34:15 -0800

    solutions added for array easy problems

---
+++ Easy/Min Cost Climbing Stairs.py
@@ -0,0 +1,15 @@
+#Leetcode 746. Min Cost Climbing Stairs
+
+#Solution - DP
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        dp = [0] * len(cost)
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i-1]+cost[i], dp[i-2]+cost[i])
+        
+        return min(dp[-1], dp[-2])
