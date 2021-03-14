630202208e4fc35efb8703d568ff0f8d504eb01b
komalupatil <ku.patil31@gmail.com>
2021-03-13 20:27:18 -0800

    BFS problem solutions added

---
+++ Easy/Minimum Depth of Binary Tree.py
@@ -0,0 +1,53 @@
+#Leetcode 111. Minimum Depth of Binary Tree
+
+#Solution - BFS
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+
+        queue = deque()
+        queue.append(root)
+        minimumTreeDepth = 0
+        while queue:
+            minimumTreeDepth += 1
+            levelSize = len(queue)
+            for _ in range(levelSize):
+                currentNode = queue.popleft()
+
+                # check if this is a leaf node
+                if not currentNode.left and not currentNode.right:
+                    return minimumTreeDepth
+
+                # insert the children of current node in the queue
+                if currentNode.left:
+                    queue.append(currentNode.left)
+                if currentNode.right:
+                    queue.append(currentNode.right)
+
+
+#Solution - DFS
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        if not root.left or not root.right:
+            return self.minDepth(root.left)+self.minDepth(root.right)+1
+        return min(self.minDepth(root.right),self.minDepth(root.left))+1
--- Medium/Binary Tree Zigzag Level Order Traversal.py
+++ Medium/Binary Tree Zigzag Level Order Traversal.py
@@ -1,5 +1,8 @@
 #Leetcode 103. Binary Tree Zigzag Level Order Traversal
 
+
+#Solution1
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -25,4 +28,38 @@ class Solution:
                     queue.append(currentNode.right)
             result.append(currentLevel[::flag])
             flag *= -1
+        return result
+
+#Solution2
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        result = []
+        if not root:
+            return result
+        
+        queue = deque()
+        queue.append(root)
+        leftToRight = True
+        while queue:
+            levelSize = len(queue)
+            currentLevel = []
+            for _ in range(levelSize): 
+                currentNode = queue.popleft()
+                if leftToRight:
+                    currentLevel.append(currentNode.val)
+                else:
+                    currentLevel.insert(0,currentNode.val)
+                if currentNode.left:
+                    queue.append(currentNode.left)
+                if currentNode.right:
+                    queue.append(currentNode.right)
+            leftToRight = not leftToRight
+            result.append(currentLevel)
         return result
