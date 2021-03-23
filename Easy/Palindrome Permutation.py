76b314db86ae48bcb79fa44aef85bc767110da79
komalupatil <ku.patil31@gmail.com>
2021-03-22 22:20:06 -0700

    solution for leetcode 266 added

---
+++ Easy/Palindrome Permutation.py
@@ -0,0 +1,31 @@
+#Leetcode 266. Palindrome Permutation
+
+#Solution
+
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        if len(s) == 0:
+            return True
+        
+        d = {}
+
+        for i in s:
+            if i in d and d[i] > 0:
+                d[i] -=1      
+            else:
+                d[i] = 1
+        
+        count = 0
+        
+        for v in d.values():
+            if count > 1:
+                return False
+            
+            if v == 1:
+                count += 1
+        
+        return count < 2
+        
+
+
+
