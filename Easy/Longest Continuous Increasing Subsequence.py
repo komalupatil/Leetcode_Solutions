a7f69168013ff82c4be44e024926c8100a341615
komalupatil <ku.patil31@gmail.com>
2021-04-05 18:28:22 -0700

    added solutions for leetcode 112 and 674

---
+++ Easy/Longest Continuous Increasing Subsequence.py
@@ -0,0 +1,18 @@
+#Leetcode 674. Longest Continuous Increasing Subsequence
+
+
+#Solution - sliding window
+
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 1
+        windowStart = 0
+        maxLength = 0
+        for windowEnd in range(1, len(nums)):
+            if nums[windowEnd] <= nums[windowEnd-1]:
+                windowStart = windowEnd
+            maxLength = max(maxLength, windowEnd - windowStart + 1)
+                
+            
+        return maxLength
