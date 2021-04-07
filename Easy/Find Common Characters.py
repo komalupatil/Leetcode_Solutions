11273d860840794817db994bb48643e68368d592
komalupatil <ku.patil31@gmail.com>
2021-04-06 19:08:15 -0700

    solution for leetcode 1002 updated

--- Easy/Find Common Characters.py
+++ Easy/Find Common Characters.py
@@ -26,3 +26,16 @@ class Solution:
         for a in A[1:]:
             counter1 &= Counter(a)
         return list(counter1.elements())
+
+#Solution3
+
+class Solution:
+    def commonChars(self, A: List[str]) -> List[str]:
+        check = list(A[0])
+        for word in A:
+            newCheck = []
+            for c in word:
+                if c in check:
+                    newCheck.append(c)
+                    check.remove(c)
+            check = newCheck
