a591acfa5b2d89f9a9243322a698ae1dbdadc7a9
komalupatil <ku.patil31@gmail.com>
2021-04-10 17:31:00 -0700

    leetcode solutions added

---
+++ Easy/Island Perimeter.py
@@ -0,0 +1,23 @@
+#Leetcode 463. Island Perimeter
+
+#Solution
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        if len(grid) == 0:
+            return 0
+        result = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    result += 4
+                
+                    if i> 0 and grid[i-1][j] == 1:
+                        result -= 1
+                    if i<len(grid)-1 and grid[i+1][j] == 1:
+                        result -= 1
+                    if j>0 and grid[i][j-1] == 1:
+                        result -= 1
+                    if j<len(grid[0])-1 and grid[i][j+1] == 1:
+                        result -= 1
+        return result
---
+++ Medium/Max Area of Island.py
@@ -0,0 +1,23 @@
+#Leetcode 695. Max Area of Island
+
+#SOlution
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                count = max(self.dfs(grid,i,j), count)
+        return count
+    
+    def dfs(self, grid, i, j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] == 0:
+            return 0
+        grid[i][j] = 0
+        
+        next_row = self.dfs(grid,i+1,j)
+        prev_row = self.dfs(grid,i-1,j)
+        next_col = self.dfs(grid,i,j+1)
+        prev_col = self.dfs(grid,i,j-1)
+        
+        return 1 + next_row + prev_row + next_col + prev_col
