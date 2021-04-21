71a19dc2d5401990954c96490233da0134c34da0
komalupatil <ku.patil31@gmail.com>
2021-04-20 20:15:44 -0700

    solution for leetcode 853 added

---
+++ Medium/Car Fleet.py
@@ -0,0 +1,28 @@
+#Leetcode 853. Car Fleet
+
+#Solution1
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = []
+        for pos, s in sorted(zip(position, speed))[::-1]:
+            print(pos,s)
+            dist = target - pos
+            if not stack:
+                stack.append(dist / s)
+            elif dist / s > stack[-1]:
+                stack.append(dist / s)
+            print(stack)
+        return len(stack)
+
+#Solution2
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        timeToTarget = [float(target-pos)/s for pos, s in sorted(zip(position,speed))]
+        result = 0
+        current = 0
+        
+        for time in timeToTarget[::-1]:
+            if time > current:
+                result +=1 
+                current = time
+        return result
