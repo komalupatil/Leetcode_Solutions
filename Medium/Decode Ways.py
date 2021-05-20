abea5a1737473cd8ca35937ddf81b9f15aeb2481
komalupatil <ku.patil31@gmail.com>
2021-05-20 00:28:13 -0700

    solutions added

--- Medium/Decode Ways.py
+++ Medium/Decode Ways.py
@@ -1,6 +1,6 @@
 #Leetcode 91. Decode Ways
 
-class Solution:
+class Solution1:
     def numDecodings(self, s: str) -> int:
         if not s:
             return 0
@@ -16,3 +16,20 @@ class Solution:
                 dp[i] += dp[i-2]
             
         return dp[len(s)]
+
+class Solution2:
+    def numDecodings(self, s: str) -> int:
+        mapping = set([str(x) for x in range(1, 27)])
+        
+        if len(s) == 1:
+            return int(s[0] in mapping)
+        
+        T = [0]*len(s)
+        T[0] = int(s[0] in mapping)
+        T[1] = (s[0] in mapping and s[1] in mapping) + (s[:2] in mapping)
+
+        for i in range(2,len(s)):
+            T[i] = (s[i] in mapping) * T[i-1] + (s[i-1:i+1] in mapping)* T[i-2]
+        
+        return T[-1]
+        
--- Medium/Minimum Path Sum.py
+++ Medium/Minimum Path Sum.py
@@ -1,6 +1,6 @@
 #Leetcode 64. Minimum Path Sum
 
-class Solution:
+class Solution1:
     def minPathSum(self, grid: List[List[int]]) -> int:
         if len(grid) == 0:
             return 0
@@ -29,3 +29,34 @@ class Solution:
                 
                 
         return dp[len(grid)-1][len(grid[0])-1]
+
+#Bottom up approach DP
+class Solution2:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        
+        result = self.helper(grid, m-1, n-1, dp)
+        
+        return result
+        
+    def helper(self, grid, m, n, dp):
+        if m == 0 and n == 0:
+            return grid[m][n]
+        
+        if m<0 or n<0:
+            return float('inf')
+        
+        if dp[m][n] != 0:
+            return dp[m][n]
+        
+        up = self.helper(grid, m-1, n, dp)
+        left = self.helper(grid, m, n-1, dp)
+        
+        result = min(up,left) + grid[m][n]
+        
+        dp[m][n] = result
+        
+        return dp[m][n]
