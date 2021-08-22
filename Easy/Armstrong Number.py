3eead597847737eda667fb80abd6f300cfb3d6f9
komalupatil <ku.patil31@gmail.com>
2021-08-22 00:05:25 -0700

    updated solutions

--- Easy/Armstrong Number.py
+++ Easy/Armstrong Number.py
@@ -1,5 +1,13 @@
 #Leetcode 1134. Armstrong Number
 
+#The k-digit number N is an Armstrong number if and only if the k-th power of each digit sums to N.
+#Given a positive integer N, return true if and only if it is an Armstrong number.
+#Example-1
+#Input: 153
+#Output: true
+#Explanation: 
+#153 is a 3-digit number, and 153 = 1^3 + 5^3 + 3^3.
+
 class Solution1:
     def isArmstrong(self, N: int) -> bool:
         result = 0
