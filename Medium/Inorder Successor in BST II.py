47b4ddd954c0086e3d82143a58aef857573036e9
komalupatil <ku.patil31@gmail.com>
2021-10-17 18:10:48 -0700

    solution to leetcode 510 added

---
+++ Medium/Inorder Successor in BST II.py
@@ -0,0 +1,25 @@
+#Leetcode 510. Inorder Successor in BST II
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def inorderSuccessor(self, node: 'Node') -> 'Node':
+        if node.right:
+            node = node.right
+            while node.left:
+                node = node.left
+            return node
+        
+        while node.parent:
+            if node.parent.left == node:
+                return node.parent
+            node = node.parent
+        return None
