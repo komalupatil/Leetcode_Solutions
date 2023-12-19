a725de9b1f9bbdc0907e56944bd147b0ab5c8d5e
Komal Patil <komalup@amazon.com>
2023-12-18 23:23:57 -0800

    adding changes to palindrome number problem

--- Easy/Palindrome Number.py
+++ Easy/Palindrome Number.py
@@ -21,9 +21,9 @@ class Solution2:
         if x < 0:
             return False
         
-        p, res = x, 0
-        while p:
-            res = res*10 + p %10
-            p //= 10
+        num, result = x, 0
+        while num:
+            result = result*10 + num%10
+            num //= 10
             
-        return res == x
+        return result == x
