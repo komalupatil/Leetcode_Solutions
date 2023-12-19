942dd0252f5dc6885c4bf9cd69822b9f304b184b
komalupatil <ku.patil31@gmail.com>
2023-12-19 00:24:23 -0800

    adding valid paranthesis solution

--- Easy/Valid Paranthesis.py
+++ Easy/Valid Paranthesis.py
@@ -1,4 +1,31 @@
-#Leetcode 20. Valid Parentheses
+# Leetcode 20. Valid Parentheses
+
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+"""
 
 class Solution1:
     def isValid(self, s: str) -> bool:
@@ -34,5 +61,27 @@ class Solution2:
                 if d[last_open] is not bracket:
                     return False
         return len(stack) ==  0
+
+
+class Solution3:
+    def isValid(self, s: str) -> bool:
+        if len(s) == 0:
+            return True
+
+        closing_brackets = []
+
+        for bracket in s:
+            if bracket == "(":
+                closing_brackets.append(")")
+            elif bracket == "{":
+                closing_brackets.append("}")
+            elif bracket == "[":
+                closing_brackets.append("]")
+            else:
+                if not closing_brackets or closing_brackets.pop() != bracket:
+                    return False
+        if closing_brackets:
+            return False
+        return True
 
 
