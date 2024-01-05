1046156aaa7af945c94606626b45a09c30cf7739
komalupatil <ku.patil31@gmail.com>
2024-01-04 21:50:04 -0800

    addiing solution for search insert position problem

--- Easy/Search Insert Position.py
+++ Easy/Search Insert Position.py
@@ -1,16 +1,44 @@
-#Leetocde 35. Search Insert Position
+# Leetocde 35. Search Insert Position
+"""
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+"""
+
+from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums) -1
+        if len(nums) == 0:
+            return 0
         
-        while l<=r:
-            mid = l + (r-l)//2
-            if target < nums[mid]:
-                r = mid -1
-            elif target >nums[mid]:
-                l = mid+1
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right-left)//2
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1 
             else:
                 return mid
-        return l
+        return left  # always when left crosses right, then left will be at the position where your target would fit in 
